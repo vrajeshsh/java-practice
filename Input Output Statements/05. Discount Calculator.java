0f899cf2c008b235ed1881ab95b9a1113801c030
Vrajesh <vrajeshshah13@gmail.com>
2022-05-21 23:26:13 -0400

    I/O statements

---
+++ Input Output Statements/05. Discount Calculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class discount {
+    public static void main(String args[]){
+        System.out.print("\033[H\033[2J");
+        try (Scanner sn = new Scanner(System.in)) {
+            System.out.print("Enter price of a computer: Rs ");
+            
+        }
+    }
+}
